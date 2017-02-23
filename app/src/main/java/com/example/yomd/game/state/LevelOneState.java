83b22372913f2837906c9178978411b9d13f4076
Ann-Marie <annmarie98.ma@gmail.com>
2017-02-23 17:46:40 +0100

    A LOT OF CHANGEEEEEES!!!!!!!

--- app/src/main/java/com/example/yomd/game/state/LevelOneState.java
+++ app/src/main/java/com/example/yomd/game/state/LevelOneState.java
@@ -1,11 +1,13 @@
 package com.example.yomd.game.state;
 
+import android.graphics.Color;
 import android.view.MotionEvent;
 
 import com.example.yomd.framework.util.Painter;
 import com.example.yomd.game.model.Ball;
 import com.example.yomd.game.model.Cloud;
 import com.example.yomd.simpleandroidgdf_monday.Assets;
+import com.example.yomd.simpleandroidgdf_monday.GameMainActivity;
 
 /**
  * Created by Ann-Marie on 2017-02-23.
@@ -15,13 +17,15 @@ public class LevelOneState extends State{
 
     private Ball ball;
     //ett par konstanter för spelarens bredd och höjd
-    private static final int PLAYER_WIDTH = 66;
-    private static final int PLAYER_HEIGHT = 92;
+    private static final int BALL_WIDTH = 158;
+    private static final int BALL_HEIGHT = 85;
 
 
     @Override
     public void init() {
-
+    ball = new Ball(GameMainActivity.GAME_WIDTH / 2 - BALL_WIDTH / 2,
+            GameMainActivity.GAME_HEIGHT - 45 - BALL_HEIGHT,
+            BALL_WIDTH,BALL_HEIGHT);
     }
 
     @Override
@@ -31,11 +35,12 @@ public class LevelOneState extends State{
 
     @Override
     public void render(Painter g) {
+        g.drawImage(Assets.fotbollsplanmeny, 0, 0);
         renderBall(g);
     }
 
     private void renderBall(Painter g){
-        g.drawImage(Assets.football, (int)ball.getX(), (int)ball.getY(), 100, 60);
+        g.drawImage(Assets.football, (int)ball.getX(), (int)ball.getY(), BALL_WIDTH, BALL_HEIGHT);
     }
 
 
--- app/src/main/java/com/example/yomd/game/state/SelectLevelState.java
+++ app/src/main/java/com/example/yomd/game/state/SelectLevelState.java
@@ -12,7 +12,7 @@ import com.example.yomd.simpleandroidgdf_monday.GameMainActivity;
 /**
  * Created by tc980615 on 2017-02-16.
  */
-public class SelectLevelState extends State{
+public class SelectLevelState extends State {
 
 
     //tre UIknappar
@@ -21,7 +21,7 @@ public class SelectLevelState extends State{
     @Override
     public void init() {
         //skapa de tre knapparna
-        level1 = new UIButton(40, 90, 120, 167, Assets.level1,Assets.level1);
+        level1 = new UIButton(40, 90, 120, 167, Assets.level1, Assets.level1);
         level2 = new UIButton(150, 90, 230, 167, Assets.level2, Assets.level2);
         level3 = new UIButton(260, 90, 340, 167, Assets.level3, Assets.level3);
     }
@@ -37,25 +37,50 @@ public class SelectLevelState extends State{
         //Rita upp welcome bilden på skärmen
         //Hämta bilden från Assets och
         //x- och y-värdet för övre vänstra hörnet är (0,0)
-        g.drawImage(Assets.levelplan,0,0);
+        g.drawImage(Assets.levelplan, 0, 0);
         renderSelect(g);
         level1.render(g);
         level2.render(g);
         level3.render(g);
-        }
+    }
 
     //rita ut poängen i spelet
-    private void renderSelect (Painter g){
+    private void renderSelect(Painter g) {
         //bestäm font och färg för texten
         g.setFont(Typeface.SANS_SERIF, 60);
         g.setColor(Color.BLACK);
         //skriv ut poängen
-        g.drawString("Select level" , 250, 60);
+        g.drawString("Select level", 250, 60);
 
     }
 
     @Override
     public boolean onTouch(MotionEvent e, int scaledX, int scaledY) {
-        return false;
+        //Kolla vad man tryckt på
+        if (e.getAction() == MotionEvent.ACTION_DOWN) {
+            level1.onTouchDown(scaledX, scaledY);
+            //scoreButton.onTouchDown(scaledX,scaledY);
+        }
+        //Reagera när man tryckt klart på skärmen
+        if (e.getAction() == MotionEvent.ACTION_UP) {
+            //om playknappen är aktiv och
+            //man släpper skärmen
+            if (level1.isPressed(scaledX, scaledY)) {
+                level1.cancel();
+                //se till att vi går till spelläget
+                //att spelet startar!
+                setCurrentState(new LevelOneState());
+
+            }//else if(scoreButton.isPressed(scaledX,scaledY)){
+            //scoreButton.cancel();
+            //logga att man tryckt ner scorebutton
+            //Log.d("MenuState", "Score Button Pressed");
+            //setCurrentState(new ScoreState());
+            else {
+                level1.cancel();
+                //scoreButton.cancel();
+            }
+        }
+        return true;
     }
 }
