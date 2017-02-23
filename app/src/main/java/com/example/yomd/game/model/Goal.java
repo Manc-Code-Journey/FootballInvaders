cedd7c164ed1fe214349ae0198e153e40c958b0d
Ann-Marie <annmarie98.ma@gmail.com>
2017-02-23 16:44:04 +0100

    change

--- app/src/main/java/com/example/yomd/game/model/Goal.java
+++ app/src/main/java/com/example/yomd/game/model/Goal.java
@@ -4,7 +4,5 @@ package com.example.yomd.game.model;
  * Created by tc980615 on 2017-02-16.
  */
 public class Goal {
-    //hej
-    //Hejjjjjj
 
 }
--- app/src/main/java/com/example/yomd/game/state/LevelOneState.java
+++ app/src/main/java/com/example/yomd/game/state/LevelOneState.java
@@ -3,12 +3,22 @@ package com.example.yomd.game.state;
 import android.view.MotionEvent;
 
 import com.example.yomd.framework.util.Painter;
+import com.example.yomd.game.model.Ball;
+import com.example.yomd.game.model.Cloud;
+import com.example.yomd.simpleandroidgdf_monday.Assets;
 
 /**
  * Created by Ann-Marie on 2017-02-23.
  */
 
 public class LevelOneState extends State{
+
+    private Ball ball;
+    //ett par konstanter för spelarens bredd och höjd
+    private static final int PLAYER_WIDTH = 66;
+    private static final int PLAYER_HEIGHT = 92;
+
+
     @Override
     public void init() {
 
@@ -21,8 +31,14 @@ public class LevelOneState extends State{
 
     @Override
     public void render(Painter g) {
-
+        renderBall(g);
     }
+
+    private void renderBall(Painter g){
+        g.drawImage(Assets.football, (int)ball.getX(), (int)ball.getY(), 100, 60);
+    }
+
+
 
     @Override
     public boolean onTouch(MotionEvent e, int scaledX, int scaledY) {
