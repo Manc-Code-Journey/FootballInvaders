dbbc0b13955c03c9a6990ea6cfa24fa21fce1da8
theodorecat <theodorecatterall@yahoo.com>
2017-03-17 16:19:05 +0100

    changes

---
+++ app/src/main/java/com/example/yomd/game/state/MainPlayState.java
@@ -0,0 +1,19 @@
+package com.example.yomd.game.state;
+
+import com.example.yomd.game.model.Ball;
+import com.example.yomd.game.model.Goal;
+
+/**
+ * Created by tc980615 on 2017-03-09.
+ */
+public class MainPlayState {
+
+    //bollen
+    private Ball ball;
+    //målet
+    private Goal goal;
+
+
+
+
+}
